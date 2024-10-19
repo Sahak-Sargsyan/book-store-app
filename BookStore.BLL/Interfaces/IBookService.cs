cdf768d3c2ada0c7a0dcb0b7b66627a70787b74f
Sahak Sargsyan <sargsyan_sahak@yahoo.com>
2024-10-20 03:37:29 +0400

    service interfaces

---
+++ BookStore.BLL/Interfaces/IBookService.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Dtos;
+
+namespace BookStore.BLL.Interfaces;
+
+public interface IBookService
+{
+    Task<IEnumerable<BookListDto>> GetAllBooksAsync();
+
+    Task<BookListDto> GetBookByIdAsync(Guid id);
+
+    Task<BookListDto> GetBookByIsbn13Async(string isbn13);
+
+    Task<IEnumerable<BookListDto>> GetBooksByAuthorsAsync(IEnumerable<Guid> authors);
+
+    Task<IEnumerable<BookListDto>> GetBooksByGenresAsync(IEnumerable<Guid> genres);
+
+    Task AddBookAsync(BookCreateDto model);
+
+    Task DeleteBookByIsbn13Async(string isbn13);
+
+    Task UpdateBookAsync(BookUpdateDto model);
+}
---
+++ BookStore.BLL/Interfaces/IPublisherService.cs
@@ -0,0 +1,18 @@
+﻿using BookStore.Dtos;
+
+namespace BookStore.BLL.Interfaces;
+
+public interface IPublisherService
+{
+    Task<IEnumerable<PublisherListDto>> GetAllPublishersAsync();
+
+    Task<PublisherListDto> GetPublisherByIdAsync(Guid id);
+
+    Task<PublisherListDto> GetPublisherByIsbn13Async(string isbn13);
+
+    Task AddPublisherAsync(PublisherCreateDto model);
+
+    Task DeletePublisherByIdAsync(Guid id);
+
+    Task UpdatePublisherAsync(PublisherUpdateDto model);
+}
