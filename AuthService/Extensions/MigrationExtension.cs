f9fd4818346ad6c7e017e7fa37a123d82845bdaa
Sahak Sargsyan <sargsyan_sahak@yahoo.com>
2025-03-22 23:31:38 +0400

    basic authentication

---
+++ AuthService/Extensions/MigrationExtension.cs
@@ -0,0 +1,15 @@
+﻿using AuthService.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Extensions
+{
+    public static class MigrationExtension
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            using AppDbContext context = scope.ServiceProvider.GetService<AppDbContext>();
+            context.Database.Migrate();
+        }
+    }
+}
---
+++ AuthService/Program.cs
@@ -0,0 +1,39 @@
+using AuthService.Data;
+using AuthService.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DB"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.ApplyMigrations();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapIdentityApi<User>();
+
+app.Run();
